a8b0bc8d3d850bd366ccd5655759fa62c19e3ea7
Julien le bo gosse <jean.michel@uda.fr>
2019-02-02 10:51:33 +0100

    first commit

---
+++ ProjetEE-ejb/build/generated-sources/ap-source-output/projetjsp/Event_.java
@@ -0,0 +1,14 @@
+package projetjsp;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-02-02T09:46:14")
+@StaticMetamodel(Event.class)
+public class Event_ { 
+
+    public static volatile SingularAttribute<Event, String> name;
+    public static volatile SingularAttribute<Event, Long> id;
+
+}
